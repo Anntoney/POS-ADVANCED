71fd5ce8cdff51d76964e33df189a6298f30b450
unknown <anjuguna95@gmail.com>
2026-02-06 15:29:10 +0300

    modified script:npm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'sunprime-pos',
-      script: 'npm',
+      script: 'node_modules/next/dist/bin/next',
       args: 'start',
       cwd: 'C:\\sunprime\\POS-ADVANCED',
       env: {
